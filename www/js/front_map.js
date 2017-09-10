f477b95d3a3fabcc688cbff1f2714dad24d4bff1
wsngit <31208362+wsngit@users.noreply.github.com>
2017-09-10 20:57:40 +0300

    General information page
    General information page

--- www/js/front_map.js
+++ www/js/front_map.js
@@ -3,15 +3,24 @@ $(document).ready(function(){
 /* jVectorMaps */
 if($("#vector_world_map").length > 0){
     
-    var jvm_wm = new jvm.WorldMap({container: $('#vector_world_map'),
+    countryMap = new jvm.WorldMap({container: $('#vector_world_map'),
                                 map: 'world_mill_en', 
                                 backgroundColor: '#B3D1FF',                                      
                                 regionsSelectable: true,
 								regionsSelectableOne: true,
-                                regionStyle: {selected: {fill: '#33414E'},
+                                regionStyle: {selected: {fill: '#1b1e24'},
                                                 initial: {fill: '#FFFFFF'}},
-                                onRegionSelected: function(){
-									window.location = "country.html?region="+jvm_wm.getSelectedRegions().toString();                                          
+                                onRegionSelected: function(){ 
+									if(countrySelectedFromMap){
+										var country = getCountryFromListISO2(countryMap.getSelectedRegions()[0]);
+										if(country!=null) {
+											setActiveCountry(country.id, false);
+											$('#vector_world_map').vectorMap('set', 'focus',curCountry.iso2);
+											updateGeneralInformation();
+										}
+										
+									}
+									countrySelectedFromMap = true;
                                 }
                                 });
                                 
